9b3c83a93fc0fd6268e8c02ecb295dc77098a8cd
Tom Hawkins <tks.hawkins11@gmail.com>
2020-10-01 16:34:53 +1000

    Can Punch 👊

--- Arm/Grabbable.gd
+++ Arm/Grabbable.gd
@@ -1,6 +1,8 @@
 extends RigidBody2D
 
 var grabbed = false
+onready var handRB = get_node("../Arm/Joint1/Joint2/Hand/HandRB")
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -16,6 +18,7 @@ func _physics_process(delta):
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT and event.is_action_pressed("mouse_left"):
+			remove_collision_exception_with(handRB)
 			print("checking collisions")
 			var bodies = $Detector.get_overlapping_bodies()
 			for b in bodies:
@@ -24,14 +27,17 @@ func _input(event):
 					grabbed = true;
 					print("grabbed item")
 		if event.button_index == BUTTON_LEFT and event.is_action_released("mouse_left"):
-			grabbed = false;
-			var LV = get_node("../Arm/Joint1/Joint2/Hand/HandRB").get_linear_velocity().normalized()
+			if grabbed:
+				grabbed = false;
+				var LV = get_node("../Arm/Joint1/Joint2/Hand/HandRB").get_linear_velocity().normalized()
 			
-			apply_impulse(Vector2(0,0), LV * 1000)
-			set_angular_velocity(LV.length() * 4)
+				apply_impulse(Vector2(0,0), LV * 1000)
+				set_angular_velocity(LV.length() * 4)
+				add_collision_exception_with(handRB)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	add_collision_exception_with(handRB)
 	pass # Replace with function body.
 
 
