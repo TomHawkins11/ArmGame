f6f40885fff67ddd6367289a6e03bb272f622247
Tom Hawkins <tks.hawkins11@gmail.com>
2020-10-06 23:27:26 +1000

    Second Arm Working

--- Arm/Scripts/Grabbable.gd
+++ Arm/Scripts/Grabbable.gd
@@ -1,8 +1,28 @@
 extends RigidBody2D
 
 var grabbed = false
-export(NodePath) var node
-onready var handRB = get_node(node)
+var game_over = false
+
+export(NodePath) var hand_sprite_node_path
+onready var hand_sprite = get_node(hand_sprite_node_path)
+
+export(NodePath) var arm_node_path
+onready var arm = get_node(arm_node_path)
+
+export(NodePath) var hand_node_path
+onready var handRB = get_node(hand_node_path)
+
+export(NodePath) var hand_node_offset_path
+onready var hand_node_offset = get_node(hand_node_offset_path)
+
+export(NodePath) var hand_NPC_node_path
+onready var hand_NPC = get_node(hand_NPC_node_path)
+
+export(NodePath) var hand_NPC_node_offset_path
+onready var hand_NPC_offset = get_node(hand_NPC_node_offset_path)
+
+var grabbing_node
+var grabbing_node_offset
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -10,14 +30,26 @@ onready var handRB = get_node(node)
 
 func _physics_process(delta):
 	if grabbed:
-		self.position = get_node("../Arm/Joint1/Joint2/Hand/GrabPos").global_position
-		self.rotation = get_node("../Arm/Joint1/Joint2/Hand").global_rotation
+		self.position = grabbing_node_offset.global_position
+		self.rotation = grabbing_node.global_rotation
 		sleeping = true
 	else:
 		sleeping = false
+		var collisions = get_colliding_bodies()
+		for c in collisions:
+			print(c.name)
+			if	c.name == "Hand_NPC_RB":
+				grabbed = true
+				game_over = true
+				play_random_grab()
+				grabbing_node = hand_NPC
+				grabbing_node_offset = hand_NPC_offset
+				hand_sprite.win()
+				arm.grab_torch()
+				
 
 func _input(event):
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and !game_over:
 		if event.button_index == BUTTON_LEFT and event.is_action_pressed("mouse_left"):
 			remove_collision_exception_with(handRB)
 			print("checking collisions")
@@ -26,23 +58,35 @@ func _input(event):
 				print(b.name)
 				if b.name == "HandRB" and grabbed == false:
 					grabbed = true;
-					var torch_sfx = load("res://Audio/Grab0" + str((randi() % 6 + 1)) + ".wav")
-					$Torch2/Grab_Sound.set_stream(torch_sfx)
-					$Torch2/Grab_Sound.play()
+					play_random_grab();
+					grabbing_node = handRB
+					grabbing_node_offset = hand_node_offset
+				
+					
 		if event.button_index == BUTTON_LEFT and event.is_action_released("mouse_left"):
 			add_collision_exception_with(handRB)
+			
 			if grabbed:
 				grabbed = false;
-				var LV = get_node("../Arm/Joint1/Joint2/Hand/HandRB").get_linear_velocity().normalized()
+				var LV = grabbing_node.get_linear_velocity().normalized()
 			
 				apply_impulse(Vector2(0,0), LV * 1000)
 				set_angular_velocity(LV.length() * 4)
 				add_collision_exception_with(handRB)
+				
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	add_collision_exception_with(handRB)
+	
+	grabbing_node = hand_node_path
+	grabbing_node_offset = hand_NPC_node_offset_path
 	pass # Replace with function body.
+	
+func play_random_grab():
+	var torch_sfx = load("res://Audio/Grab0" + str((randi() % 6 + 1)) + ".wav")
+	$Torch2/Grab_Sound.set_stream(torch_sfx)
+	$Torch2/Grab_Sound.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Arm/Scripts/SpriteSwap.gd
+++ Arm/Scripts/SpriteSwap.gd
@@ -5,6 +5,8 @@ export(String, FILE, "*.png") var sprite_new_path
 onready var spriteToSwap = load(sprite_to_swap_path)
 onready var spriteNew = load(sprite_new_path)
 
+var grabbed = false
+
 func switch_sprite(sprite):
 	texture = sprite
 
@@ -12,7 +14,7 @@ func switch_sprite(sprite):
 # var a = 2
 # var b = "text"
 func _input(event):
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and grabbed == false:
 		if event.button_index == BUTTON_LEFT and event.pressed:
 			switch_sprite(spriteNew)
 		else:
@@ -23,6 +25,8 @@ func _ready():
 	
 	pass # Replace with function body.
 
+func win():
+	switch_sprite(spriteNew)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
