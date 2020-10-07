a7f20e831c61b2cdd2dfaa7d52b2be2e9e658293
Tom Hawkins <tks.hawkins11@gmail.com>
2020-10-07 20:47:23 +1000

    Torch Reset Working

--- Arm/Boundaries.gd
+++ Arm/Boundaries.gd
@@ -6,6 +6,7 @@ onready var upper_bounds:Position2D = get_node(upper_bounds_path)
 export(NodePath) var lower_bounds_path
 onready var lower_bounds:Position2D = get_node(lower_bounds_path)
 
+var reset_state = false
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -17,10 +18,14 @@ func _ready():
 	pass # Replace with function body.
 	
 func get_all_rb():
-	var n=[]
-	for c in ():
-    	if c extends DesiredClass:
-        	n.append(c)
+	for c in (get_tree().get_nodes_in_group("RB")):
+    	if c is RigidBody2D:
+        	if get_pos(c).y < lower_bounds.y:
+				c.
+				
+
+    
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
